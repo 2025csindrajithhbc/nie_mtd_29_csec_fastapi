86dab57ac1fb2598a589aa53c921f155511e68bc
indrajith H B <2025cs_indrajithhb_c@nie.ac.in>
2026-09-22 12:37:25 +0530

    fastapi

--- Day_02/main.py
+++ Day_02/main.py
@@ -1,3 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+@app.get("/") #paths (tasks)
+
+def home():
+    return {"message" : "E-commerce Customer Support System - server"} #Seriliess
+
+db = {
+    1:{"id" : 1,"title" : "Damaged item",
+       "description" : "Resolation problem",
+       "category" : "Hardware",
+       "status" : "New"},
+    2:{"id" : 2,"title" : "Item missing",
+       "description" : "refund " ,
+       "category" : "empty delivity",
+       "status" : "New"}
+}
+@app.get("/tickets")
+def ticket_read_all():
+    return list(db.values())
+
+@app.get("/tickets/{id}")
+def ticket_read_by_id(id : int):
+    return db[id]
