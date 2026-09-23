7ba388b27069504990bbdc562cea905e51ccea6a
indrajith H B <2025cs_indrajithhb_c@nie.ac.in>
2026-09-23 15:38:39 +0530

    fastapi

--- Day_03/main.py
+++ Day_03/main.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel
 from pymongo import MongoClient
 from bson import ObjectId
 
+import jwt
+from fastapi.security import  OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pwdlib import PasswordHash
+from datetime import datetime,timedelta,timezone
+
 # app
 app = FastAPI()
 
@@ -11,6 +16,11 @@ URL = "mongodb://127.0.0.1:27017"
 client = MongoClient(URL)
 db = client["Support_ticket_db"]
 ticket_collection = db["tickets"]
+user_collection = db["users"]
+
+#security config
+password_hash = PasswordHash.recommended()
+SECRET_KEY= 
 
 # pydantic schama
 
@@ -22,7 +32,18 @@ class TicketCreate(BaseModel):
 
 class TicketResponse(TicketCreate):
     id : str
+
+#....users
+class UserCreate(BaseModel):
+    username : str
+    password : str
+    role : int
     
+#....token
+class TokenResponse(BaseModel):
+    access_token : str
+    token_type : str
+
 # helper
 def ticket_helper(ticket_doc):
     return {
@@ -32,7 +53,17 @@ def ticket_helper(ticket_doc):
         "catagory" : ticket_doc["catagory"],
         "status" : ticket_doc["status"]
     } 
-
+#....user_helper
+def user_helper(user):
+    return{
+        "id" : str(user["_id"]),
+        "username" : user["username"],
+        "role" : user["role"]
+    }
+    
+# create jwt
+def create_token(username : str,role : int):
+    expire = datetime.now(timezone.utc) + timedelta(minutes=TOKEN_EXPIRE_MINS)
 #apis - CRUD ,read all, read by id , update ,delete
 @app.post("/tickets",status_code=201,response_model=TicketResponse)
 def ticket_create(payload : TicketCreate):
@@ -49,7 +80,7 @@ def ticket_read_all():
 
 @app.get("/tickets/{id}",response_model=TicketResponse)
 def ticket_read_by_id(id : str):
-    if not object.is_valid(id):
+    if not ObjectId.is_valid(id):
         raise HTTPException(detail="Invalid Ticket ID",status_code=403)
     doc = ticket_collection.find_one({"_id" : ObjectId(id)})
     if not doc:
@@ -58,10 +89,10 @@ def ticket_read_by_id(id : str):
 
 @app.put("/tickets/{id}",response_model=TicketResponse)
 def ticket_update(id : str , payload:TicketCreate):
-    if not object.is_valid(id):
+    if not ObjectId.is_valid(id):
         raise HTTPException(detail="Invalid Ticket ID",status_code=403)
     ticket_dict = payload.model_dump()
-    result = ticket_collection.update_one({"_id" : ObjectId(id)},
+    result = ticket_collection.update_many({"_id" : ObjectId(id)},
                                           {"$set" : ticket_dict})
     if result.matched_count == 0:
         raise HTTPException(detail="Ticket Not Found", status_code=404)
@@ -70,9 +101,9 @@ def ticket_update(id : str , payload:TicketCreate):
     
 @app.delete("/tickets/{id}")
 def ticket_delete(id:str):
-    if not object.is_valid(id):
+    if not ObjectId.is_valid(id):
         raise HTTPException(detail="Invalid Ticket ID",status_code=403)
     result = ticket_collection.delete_one({"_id" : ObjectId(id)})
-    if result.matched_count == 0:
+    if result.deleted_count == 0:
         raise HTTPException(detail="Ticket Not Found", status_code=404)
     return {"message" : "Ticket Deleted Succcessfuly"}
