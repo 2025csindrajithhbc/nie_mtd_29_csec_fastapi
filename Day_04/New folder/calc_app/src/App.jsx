882ca9cc135778b684e53f8bd40756aa7e0bc502
Indrajith H B <2025cs_indrajithhb_c@nie.ac.in>
2026-09-25 09:14:40 +0530

    fastapi

--- Day_04/New folder/calc_app/src/App.jsx
+++ Day_04/New folder/calc_app/src/App.jsx
@@ -6,8 +6,12 @@ function App() {
   return (
     <>
       <h1>Hello React</h1>
+      <hr></hr>
       <About />
+      <hr></hr>
       <Square />
+      <hr></hr>
+      <h5>Sum Calculation</h5>
       <SumCal />
     </>
   )
