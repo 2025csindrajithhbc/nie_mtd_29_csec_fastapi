111584c9f9d812e864a47d536a26acd7f698c416
Indrajith H B <2025cs_indrajithhb_c@nie.ac.in>
2026-09-25 09:14:48 +0530

    Fastapi

--- Day_04/New folder/calc_app/src/SumCal.jsx
+++ Day_04/New folder/calc_app/src/SumCal.jsx
@@ -18,7 +18,7 @@ export default function SumCal() {
                     value={num2}
                     onChange={(e) => {setNum2(parseInt(e.target.value)); }}
                 /> </p>
-            <p><button onClick={() => { setSqr(Number(num1) * Number(num2)); }}>Calculate</button></p>
+            <p><button onClick={() => { setResult(Number(num1) + Number(num2)); }}>Calculate</button></p>
             <p>SUm of {num1} and {num2} is {result}</p>
         </>
     )
